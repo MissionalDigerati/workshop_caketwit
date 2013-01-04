5491ab19dc44d403013bd2bf398dae639c427efb
Codemis <johnathan@missionaldigerati.com>
2013-01-04 11:19:37 -0800

    Updated the Security.salt.

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,7 +184,7 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'AWWEShERR211128776554AW');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
